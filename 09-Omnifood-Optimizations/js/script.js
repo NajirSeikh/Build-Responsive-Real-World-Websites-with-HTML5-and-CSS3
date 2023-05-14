ae6fdfd3b185902a851d39a13520194cc5b82ada
NajirSeikh <ns.soft.97@gmail.com>
2023-05-14 08:18:18 +0530

    Implementing Smooth Scrolling

--- 09-Omnifood-Optimizations/js/script.js
+++ 09-Omnifood-Optimizations/js/script.js
@@ -27,6 +27,38 @@ btnNavEl.addEventListener('click', function () {
 });
 
 ///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+const allLinks = document.querySelectorAll('a:link');
+// console.log(allLinks);
+
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    // console.log(e);
+
+    e.preventDefault();
+    const href = link.getAttribute('href');
+    // console.log(href);
+
+    // Scroll back to top
+    if (href === '#')
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+
+    // Scroll to other links
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains('main-nav-link'))
+      headerEl.classList.toggle('nav-open');
+  });
+});
+
+///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement('div');
